4bb19d81903364177a6b29fa8d0cb704f2231bba
Nilson Jorge Leão Júnior <116318343+NilsonJorge@users.noreply.github.com>
2024-05-21 12:14:58 -0300

    final-version

--- frontend/src/components/LikeContainer.jsx
+++ frontend/src/components/LikeContainer.jsx
@@ -1,18 +1,25 @@
 import './LikeContainer.css'
 
 import {BsHeart, BsHeartFill} from 'react-icons/bs'
+import { FaRegCommentDots } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const LikeContainer = ({photo, user, handleLike, handleDesLike}) => {
   return (
     <div className='like'>
         {photo.likes && user && (
             <>
+                
                 {photo.likes.includes(user._id) ? (
                     <BsHeartFill onClick={() => handleDesLike(photo)}/>
                 ) : (
                     <BsHeart onClick={() => handleLike(photo)}/>
                 )}
                 <p>{photo.likes.length} like(s)</p>
+
+                <Link to={`/photos/${photo._id}`}>
+                    <FaRegCommentDots/> 
+                </Link>
             </>
         ) }
     </div>
